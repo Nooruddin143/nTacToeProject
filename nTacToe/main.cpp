6034f237870b0af6d143eccb6c166dac2418e0e1
Nooruddin143 <125521617+Nooruddin143@users.noreply.github.com>
2024-02-25 17:03:28 -0800

    Initial Commit

---
+++ nTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  nTacToe
+//
+//  Created by Nooruddin Mohammadi on 2/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
